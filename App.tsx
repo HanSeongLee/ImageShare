fe68fb190c9e608947ff96aea70b79bf3194dc0b
HanSeongLee <jkhanseong@gmail.com>
2022-07-30 20:01:51 +0900

    ADD | Logo image

--- App.tsx
+++ App.tsx
@@ -1,25 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, Text, View } from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={{
-          color: '#888',
-          fontSize: 18,
-      }}>
-        Text to share a photo from your phone with a friend, just press the button below!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Image source={logo}
+                   style={{
+                       width: 305,
+                       height: 159,
+                   }}
+            />
+
+            <Text style={{
+                color: '#888',
+                fontSize: 18,
+            }}>
+                Text to share a photo from your phone with a friend, just press the button below!
+            </Text>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
